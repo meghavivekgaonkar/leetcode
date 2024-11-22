45839545fba009833ec2c2ff277b5a9123e0874e
Megha Vivek Gaonkar <meghavivekgaonkar@gmail.com>
2024-11-22 17:43:48 -0500

    UnivaluedBT

---
+++ com/binarytree/problem/UnivaluedBinaryTree.java
@@ -0,0 +1,18 @@
+package com.binarytree.problem;
+
+public class UnivaluedBinaryTree {
+    public boolean isUnivalTree(TreeNode root) {
+        return isUnivaluedTree(root,root.val);
+    }
+
+    //recurssively check if the root node has the same value
+    //update the root node to left and right 
+    public boolean isUnivaluedTree(TreeNode root, int val){
+
+        if(root==null){
+             return true;
+        }
+        return root.val==val && isUnivaluedTree(root.left, val) && isUnivaluedTree(root.right,val);
+      
+    }
+}
