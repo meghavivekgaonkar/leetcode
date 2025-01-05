079ab9b5083eda3676c54c465618b23900424abc
Megha Vivek Gaonkar <meghavivekgaonkar@gmail.com>
2025-01-04 22:03:53 -0500

    min cost of tickets

--- MinimumCostOfTickets.java
+++ MinimumCostOfTickets.java
@@ -2,35 +2,27 @@ import java.util.Arrays;
 import java.util.HashSet;
 
 class MinimumCostOfTickets{
-    public static HashSet<Integer> isTravelNeeded = new HashSet<>(); // 1 2 3 4 5 6 7 8 9 10 30 31
-
-    public static int solve(int[] dp, int[] days, int[] costs, int currDay) {
-        if (currDay > days[days.length - 1]) {
-            return 0;
-        }
-        if (!isTravelNeeded.contains(currDay)) {
-            return solve(dp, days, costs, currDay + 1);
-        }
-        if (dp[currDay] != -1) {
-            return dp[currDay];
-        }
-
-        int oneDay = costs[0] + solve(dp, days, costs, currDay + 1); //  24
-        int sevenDay = costs[1] + solve(dp, days, costs, currDay + 7); // 21
-        int thirtyDay = costs[2] + solve(dp, days, costs, currDay + 30); // 30
-
-        // Store the cost with the minimum of the three options.
-        return dp[currDay] = Math.min(oneDay, Math.min(sevenDay, thirtyDay));
-    }
-
     public static int mincostTickets(int[] days, int[] costs) {
-        int lastDay = days[days.length - 1]; // 31
-        int dp[] = new int[lastDay + 1]; //32
-        Arrays.fill(dp, -1);
-        for (int day : days) {
-            isTravelNeeded.add(day);
+        int lastDay = days[days.length - 1];
+        int dp[] = new int[lastDay + 1];
+        Arrays.fill(dp, 0);
+
+        int i = 0;
+        for (int day = 1; day <= lastDay; day++) {
+            // If we don't need to travel on this day, the cost won't change.
+            if (day < days[i]) {
+                dp[day] = dp[day - 1];
+            } else {
+                // Buy a pass on this day, and move on to the next travel day.
+                i++;
+                // Store the cost with the minimum of the three options.
+                dp[day] = Math.min(dp[day - 1] + costs[0],
+                        Math.min(dp[Math.max(0, day - 7)] + costs[1],
+                                dp[Math.max(0, day - 30)] + costs[2]));
+            }
         }
-        return solve(dp, days, costs, 1);
+
+        return dp[lastDay];
     }
 
 
